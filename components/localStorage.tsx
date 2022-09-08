1b9616113594f59889e883d87d2400e8bf1c472c
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2022-09-08 15:19:51 -0700

    Description localStorage

--- components/localStorage.tsx
+++ components/localStorage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 const LOCAL_STORAGE_KEY_NAME = 'CompanyName';
-const LOCAL_STORAGE_KEY_DESCRIPTION = 'Advertisement Description';
+const LOCAL_STORAGE_KEY_DESCRIPTION = 'AdvertisementDescription';
 
 export default function Local({ company, setCompany, description, setDescription }) {
     
@@ -16,8 +16,16 @@ export default function Local({ company, setCompany, description, setDescription
         JSON.stringify(company))
       }, [company]);
 
-    
     //Advertisement Description
+    useEffect(() => {
+        const storedDescription = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DESCRIPTION))
+        if (storedDescription) setDescription(storedDescription)
+      }, [])
+    
+      useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_DESCRIPTION, 
+        JSON.stringify(description))
+      }, [description]);
     
     return (
         <>
