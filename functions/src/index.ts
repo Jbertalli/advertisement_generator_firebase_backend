28c84c31a2a025236e4fa9c18c1bdceb59d8ec49
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2022-09-09 14:10:45 -0700

    function that triggers whenever user created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+//function that triggers whenever a firebase user is created
+export const createUserDocument = functions.auth.user().onCreate((user) => {
+    //add document into user collection whenever new user signs in 
+    db.collection("users")
+    .doc(user.uid)
+    .set(JSON.parse(JSON.stringify(user)));
+});
