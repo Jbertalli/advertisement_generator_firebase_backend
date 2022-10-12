2e90414293c3e01cbec86aa69894c5fc447f0f7f
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2022-10-12 01:01:54 -0700

    fix hydration errors

--- pages/history.tsx
+++ pages/history.tsx
@@ -14,7 +14,7 @@ export default function History() {
     const [userData, setUserData] = useState([]);
     const [tableMargin, setTableMargin] = useState<string>('0px  40px 0px 40px');
     const [tableScale, setTableScale] = useState<string>('');
-    const [user] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     // console.log(user.email);
     const router: NextRouter = useRouter();
 
@@ -84,108 +84,116 @@ export default function History() {
                 <meta name="description" content="information" />
             </Head>
             <Header />
-            <div style={{ background: 'linear-gradient(to top, blue, #125CA180)', height: '100vh' }}>
-                {/* <div style={{ transform: 'translateY(-15px)', padding: '70px', position: 'relative', zIndex: '10', maxWidth: '600px' }}>
-                    <div style={{ 
-                        display: 'flex', 
-                        justifyContent: 'center', 
-                        background: 'white', 
-                        color: '#125CA1',  
-                        height: '60px', 
-                        fontSize: '30px', 
-                        fontWeight: '300',
-                        marginBottom: '30px',
-                        marginTop: '-30px',
-                        borderRadius: '3px',
-                        boxShadow: '2px 2px 15px black',
-                        position: 'relative'
-                    }}>
-                        <div style={{ transform: 'translateY(20px)' }}>
-                            Saved Advertisements
-                        </div>
-                    </div>
-                </div> */}
-                <div style={{ margin: `${tableMargin}`, transform: `${tableScale}`, position: 'relative', zIndex: '0', fontSize: '20px', display: 'flex', justifyContent: 'center' }}>
-                    <Table 
-                        striped 
-                        celled 
-                        textAlign="center" 
-                        unstackable 
-                        style={{ 
-                            maxWidth: '600px', 
-                            height: '60vh',
-                            color: '#125CA1', 
+            {loading ? (
+            <>
+                Hello
+            </>
+            ):(
+            <>
+                <div style={{ background: 'linear-gradient(to top, blue, #125CA180)', height: '100vh' }}>
+                    {/* <div style={{ transform: 'translateY(-15px)', padding: '70px', position: 'relative', zIndex: '10', maxWidth: '600px' }}>
+                        <div style={{ 
+                            display: 'flex', 
+                            justifyContent: 'center', 
+                            background: 'white', 
+                            color: '#125CA1',  
+                            height: '60px', 
+                            fontSize: '30px', 
+                            fontWeight: '300',
+                            marginBottom: '30px',
+                            marginTop: '-30px',
+                            borderRadius: '3px',
                             boxShadow: '2px 2px 15px black',
-                            background: 'rgb(255, 255, 255, 0.8', 
-                            transform: 'translateY(60px)',
-                        }}
-                    >
-                        <Table.Body>
-                            <Table.Header style={{ fontSize: '30px', transform: 'translateX(50%)' }}>
-                                <div style={{ padding: '10px', lineHeight: '30px' }}>
-                                    Saved Advertisements
-                                </div>
-                            </Table.Header>
-                            <Table.Row>
-                                <Table.Cell>
-                                    Database ID
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {dbId}
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell style={{ width: '50%' }}>
-                                    Company Name
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {dbCompany}
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>
-                                    Company Description
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {dbDescription}
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>
-                                    Image Height 
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {dbHeight}
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>
-                                    Image Left 
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {dbLeft}
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>
-                                    Image Top 
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {dbTop}
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell>
-                                    Image Width 
-                                </Table.Cell>
-                                <Table.Cell>
-                                    {dbWidth}
-                                </Table.Cell>
-                            </Table.Row>
-                        </Table.Body>
-                    </Table>      
+                            position: 'relative'
+                        }}>
+                            <div style={{ transform: 'translateY(20px)' }}>
+                                Saved Advertisements
+                            </div>
+                        </div>
+                    </div> */}
+                    <div style={{ margin: `${tableMargin}`, transform: `${tableScale}`, position: 'relative', zIndex: '0', fontSize: '20px', display: 'flex', justifyContent: 'center' }}>
+                        <Table 
+                            striped 
+                            celled 
+                            textAlign="center" 
+                            unstackable 
+                            style={{ 
+                                maxWidth: '600px', 
+                                height: '60vh',
+                                color: '#125CA1', 
+                                boxShadow: '2px 2px 15px black',
+                                background: 'rgb(255, 255, 255, 0.8', 
+                                transform: 'translateY(60px)',
+                            }}
+                        >
+                            <Table.Body>
+                                <Table.Header style={{ fontSize: '30px', transform: 'translateX(50%)' }}>
+                                    <div style={{ padding: '10px', lineHeight: '30px' }}>
+                                        Saved Advertisements
+                                    </div>
+                                </Table.Header>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        Database ID
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {dbId}
+                                    </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell style={{ width: '50%' }}>
+                                        Company Name
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {dbCompany}
+                                    </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        Company Description
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {dbDescription}
+                                    </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        Image Height 
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {dbHeight}
+                                    </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        Image Left 
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {dbLeft}
+                                    </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        Image Top 
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {dbTop}
+                                    </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        Image Width 
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {dbWidth}
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Body>
+                        </Table>      
+                    </div>
                 </div>
-            </div>
+            </>
+            )}
         </>
     );
 }
