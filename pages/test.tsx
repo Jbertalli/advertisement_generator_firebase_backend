8e4dd8f441006c0bad70b9fbff3252fe2354f4cb
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2022-10-16 20:22:37 -0700

    Test setup

---
+++ pages/test.tsx
@@ -0,0 +1,137 @@
+import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
+import { Button, Icon, Input } from 'semantic-ui-react';
+
+export default function Test() {
+    const [newQuestion, setNewQuestion] = useState<boolean>(true);
+    const [question, setQuestion] = useState<string>('');
+    const [answer, setAnswer] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [date, setDate] = useState<string>('');
+    const [nameClicked, setNameClicked] = useState<boolean>(false);
+    const [dateClicked, setDateClicked] = useState<boolean>(false);
+
+    return (
+        <>
+            <Head>
+                <title>Test Generator</title>
+                <meta name="description" content="test" />
+            </Head>
+            {!nameClicked ? (
+            <>
+                <div>
+                    <div>
+                        Name
+                    </div>
+                    <input 
+                        placeholder="Name"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    {(name.length > 0) ? (
+                    <>
+                        <Button onClick= {() => setNameClicked(true)}>
+                            Save
+                        </Button>
+                    </>
+                    ): null}
+                </div>
+            </>
+            ):(
+            <>
+                {name}
+            </>
+            )}
+            {!dateClicked ? (
+            <>
+                <div>
+                    <div>
+                        Date
+                    </div>
+                    <input 
+                        placeholder="Date"
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                    {(date.length > 0) ? (
+                    <>
+                        <Button onClick={() => setDateClicked(true)}>
+                            Save
+                        </Button>
+                    </>
+                    ):null}
+                </div>
+            </>
+            ):(
+            <>
+                {date}
+            </>
+            )}
+            
+            <div>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    Title
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <input 
+                        placeholder="Title"
+                    />
+                </div>
+                <Button>
+                    Save
+                </Button>
+            </div>
+            {newQuestion ? (
+            <>
+                <Button onClick={() => setNewQuestion(false)}>
+                    <Icon
+                        name="plus"
+                    />
+                    Add Question
+                </Button>
+            </>
+            ):(
+            <>
+                <Button onClick={() => {setNewQuestion(true), setQuestion(''), setAnswer('')}}>
+                    <Icon
+                        name="plus"
+                    />
+                    Delete Question
+                </Button>
+            </>
+            )}
+            <div>
+                {!newQuestion ? (
+                <>
+                    <div>
+                        Question
+                        <Input
+                            placeholder="Question"
+                            onChange={(e) => setQuestion(e.target.value)}
+                        />
+                    </div>
+                    
+                    <div>
+                        Answer
+                        <Input 
+
+                            placeholder="Answer"
+                            onChange={(e) => setAnswer(e.target.value)}
+                        />
+                    </div>
+                </>
+                ): null}
+            </div>
+            <div>
+                {question}
+            </div>
+            <div>
+                {answer}
+            </div>
+            <Button color="blue" style={{ transform: 'translateY(600px)' }}>
+                <Icon
+                    name="check"
+                />
+                Grade
+            </Button>
+        </>
+    );
+}
