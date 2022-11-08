dd957950fbe22d762d5bd7ad1f81323c903e28d6
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2022-11-07 20:26:09 -0800

    redux setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { store } from '../store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout {...pageProps}>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
