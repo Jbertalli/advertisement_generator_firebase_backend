b66de335c532162138658f6b1bb9d7dd430b82d4
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2022-11-25 17:44:09 -0800

    custom Head and react import

--- pages/custom.tsx
+++ pages/custom.tsx
@@ -1,6 +1,13 @@
+import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
+
 export default function Custom() {
     return (
         <>
+            <Head>
+                <title>Custom Advertisement Generator</title>
+                <meta name="description" content="earnandtrade, advertisement" />
+            </Head>
             <div>
                 Hello
             </div>
