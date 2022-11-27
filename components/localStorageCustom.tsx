ec8d297c3d273e3c20e53bef3812272927543569
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2022-11-26 18:15:37 -0800

    localStorage refactor

---
+++ components/localStorageCustom.tsx
@@ -0,0 +1,163 @@
+import React, { useEffect } from 'react';
+
+const LOCAL_STORAGE_KEY_COMPANY = 'Company';
+const LOCAL_STORAGE_KEY_COMPANY_FONT = 'Company_Font';
+const LOCAL_STORAGE_KEY_COMPANY_WEIGHT = 'Company_Weight';
+const LOCAL_STORAGE_KEY_DESCRIPTION = 'Description';
+const LOCAL_STORAGE_KEY_DESCRIPTION_FONT = 'Description_Font';
+const LOCAL_STORAGE_KEY_DESCRIPTION_WEIGHT = 'Description_Weight';
+const LOCAL_STORAGE_KEY_BORDER_WIDTH = 'Border_Width';
+const LOCAL_STORAGE_KEY_BORDER_COLOR = 'Border_Color';
+const LOCAL_STORAGE_KEY_COLOR = 'Color';
+const LOCAL_STORAGE_KEY_BACKGROUND = 'Background';
+const LOCAL_STORAGE_KEY_CUSTOM_IMAGE = 'Custom_Image';
+const LOCAL_STORAGE_KEY_IMAGE_WIDTH = 'Image_Width';
+const LOCAL_STORAGE_KEY_IMAGE_HEIGHT = 'Image_Height';
+const LOCAL_STORAGE_KEY_IMAGE_ROTATION = 'Image_Rotation';
+
+export default function LocalCustom({ company, setCompany, companyFontSize, setCompanyFontSize, companyFontWeight, setCompanyFontWeight, description, setDescription, descriptionFontSize, setDescriptionFontSize, descriptionFontWeight, setDescriptionFontWeight, borderWidth, setBorderWidth, borderColor, setBorderColor, color, setColor, backgroundColor, setBackgroundColor, mediaPreview, setMediaPreview, image, setImage, imageWidth, setImageWidth, imageHeight, setImageHeight, imageRotation, setImageRotation }) {
+
+    useEffect(() => {
+        const storedCompany = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COMPANY))
+        if (storedCompany) setCompany(storedCompany)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_COMPANY, 
+        JSON.stringify(company))
+    }, [company]);
+
+    useEffect(() => {
+        const storedCompanyFontSize = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COMPANY_FONT))
+        if (storedCompanyFontSize) setCompanyFontSize(storedCompanyFontSize)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_COMPANY_FONT, 
+        JSON.stringify(companyFontSize))
+    }, [companyFontSize]);
+
+    useEffect(() => {
+        const storedCompanyFontWeight = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COMPANY_WEIGHT))
+        if (storedCompanyFontWeight) setCompanyFontWeight(storedCompanyFontWeight)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_COMPANY_WEIGHT, 
+        JSON.stringify(companyFontWeight))
+    }, [companyFontWeight]);
+
+    useEffect(() => {
+        const storedDescription = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DESCRIPTION))
+        if (storedDescription) setDescription(storedDescription)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_DESCRIPTION, 
+        JSON.stringify(description))
+    }, [description]);
+
+    useEffect(() => {
+        const storedDescriptionFontSize = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DESCRIPTION_FONT))
+        if (storedDescriptionFontSize) setDescriptionFontSize(storedDescriptionFontSize)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_DESCRIPTION_FONT, 
+        JSON.stringify(descriptionFontSize))
+    }, [descriptionFontSize]);
+
+    useEffect(() => {
+        const storedDescriptionFontWeight = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DESCRIPTION_WEIGHT))
+        if (storedDescriptionFontWeight) setDescriptionFontWeight(storedDescriptionFontWeight)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_DESCRIPTION_WEIGHT, 
+        JSON.stringify(descriptionFontWeight))
+    }, [descriptionFontWeight]);
+
+    useEffect(() => {
+        const storedBorderWidth = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_BORDER_WIDTH))
+        if (storedBorderWidth) setBorderWidth(storedBorderWidth)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_BORDER_WIDTH, 
+        JSON.stringify(borderWidth))
+    }, [borderWidth]);
+
+    useEffect(() => {
+        const storedBorderColor = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_BORDER_COLOR))
+        if (storedBorderColor) setBorderColor(storedBorderColor)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_BORDER_COLOR, 
+        JSON.stringify(borderColor))
+    }, [borderColor]);
+
+    useEffect(() => {
+        const storedColor = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COLOR))
+        if (storedColor) setColor(storedColor)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_COLOR, 
+        JSON.stringify(color))
+    }, [color]);
+
+    useEffect(() => {
+        const storedBackgroundColor = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_BACKGROUND))
+        if (storedBackgroundColor) setBackgroundColor(storedBackgroundColor)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_BACKGROUND, 
+        JSON.stringify(backgroundColor))
+    }, [backgroundColor]);
+
+    useEffect(() => {
+        const storedImage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_CUSTOM_IMAGE))
+        if (storedImage) setMediaPreview(storedImage)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_CUSTOM_IMAGE, 
+        JSON.stringify(mediaPreview))
+    }, [mediaPreview]);
+
+    useEffect(() => {
+        const storedImageWidth = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IMAGE_WIDTH))
+        if (storedImageWidth) setImageWidth(storedImageWidth)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_IMAGE_WIDTH, 
+        JSON.stringify(imageWidth))
+    }, [imageWidth]);
+
+    useEffect(() => {
+        const storedImageHeight = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IMAGE_HEIGHT))
+        if (storedImageHeight) setImageHeight(storedImageHeight)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_IMAGE_HEIGHT, 
+        JSON.stringify(imageHeight))
+    }, [imageHeight]);
+
+    useEffect(() => {
+        const storedImageRotation = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IMAGE_ROTATION))
+        if (storedImageRotation) setImageRotation(storedImageRotation)
+      }, [])
+    
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY_IMAGE_ROTATION, 
+        JSON.stringify(imageRotation))
+    }, [imageRotation]);
+
+    return (
+        <></>
+    );
+}
--- pages/custom.tsx
+++ pages/custom.tsx
@@ -1,23 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/advertisement.module.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Divider, Container, Segment } from 'semantic-ui-react';
 import Draggable from 'react-draggable';
-
-const LOCAL_STORAGE_KEY_COMPANY = 'Company';
-const LOCAL_STORAGE_KEY_COMPANY_FONT = 'Company_Font';
-const LOCAL_STORAGE_KEY_COMPANY_WEIGHT = 'Company_Weight';
-const LOCAL_STORAGE_KEY_DESCRIPTION = 'Description';
-const LOCAL_STORAGE_KEY_DESCRIPTION_FONT = 'Description_Font';
-const LOCAL_STORAGE_KEY_DESCRIPTION_WEIGHT = 'Description_Weight';
-const LOCAL_STORAGE_KEY_BORDER_WIDTH = 'Border_Width';
-const LOCAL_STORAGE_KEY_BORDER_COLOR = 'Border_Color';
-const LOCAL_STORAGE_KEY_COLOR = 'Color';
-const LOCAL_STORAGE_KEY_BACKGROUND = 'Background';
-const LOCAL_STORAGE_KEY_CUSTOM_IMAGE = 'Custom_Image';
-const LOCAL_STORAGE_KEY_IMAGE_WIDTH = 'Image_Width';
-const LOCAL_STORAGE_KEY_IMAGE_HEIGHT = 'Image_Height';
-const LOCAL_STORAGE_KEY_IMAGE_ROTATION = 'Image_Rotation';
+import LocalCustom from '../components/localStorageCustom';
 
 export default function Custom() {
     const [company, setCompany] = useState<string>('');
@@ -34,7 +20,7 @@ export default function Custom() {
     const [image, setImage] = useState({name: '', media: ''});
     const [imageWidth, setImageWidth] = useState<string>('');
     const [imageHeight, setImageHeight] = useState<string>('');
-    const [imageRotation, setImageRotation] = useState<string>('0');
+    const [imageRotation, setImageRotation] = useState<string>('');
 
     function handleChange(event) {
         const { name, files } = event.target;
@@ -52,152 +38,13 @@ export default function Custom() {
 
     console.log(mediaPreview);
 
-    useEffect(() => {
-        const storedCompany = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COMPANY))
-        if (storedCompany) setCompany(storedCompany)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_COMPANY, 
-        JSON.stringify(company))
-    }, [company]);
-
-    useEffect(() => {
-        const storedCompanyFontSize = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COMPANY_FONT))
-        if (storedCompanyFontSize) setCompanyFontSize(storedCompanyFontSize)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_COMPANY_FONT, 
-        JSON.stringify(companyFontSize))
-    }, [companyFontSize]);
-
-    useEffect(() => {
-        const storedCompanyFontWeight = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COMPANY_WEIGHT))
-        if (storedCompanyFontWeight) setCompanyFontWeight(storedCompanyFontWeight)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_COMPANY_WEIGHT, 
-        JSON.stringify(companyFontWeight))
-    }, [companyFontWeight]);
-
-    useEffect(() => {
-        const storedDescription = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DESCRIPTION))
-        if (storedDescription) setDescription(storedDescription)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_DESCRIPTION, 
-        JSON.stringify(description))
-    }, [description]);
-
-    useEffect(() => {
-        const storedDescriptionFontSize = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DESCRIPTION_FONT))
-        if (storedDescriptionFontSize) setDescriptionFontSize(storedDescriptionFontSize)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_DESCRIPTION_FONT, 
-        JSON.stringify(descriptionFontSize))
-    }, [descriptionFontSize]);
-
-    useEffect(() => {
-        const storedDescriptionFontWeight = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DESCRIPTION_WEIGHT))
-        if (storedDescriptionFontWeight) setDescriptionFontWeight(storedDescriptionFontWeight)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_DESCRIPTION_WEIGHT, 
-        JSON.stringify(descriptionFontWeight))
-    }, [descriptionFontWeight]);
-
-    useEffect(() => {
-        const storedBorderWidth = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_BORDER_WIDTH))
-        if (storedBorderWidth) setBorderWidth(storedBorderWidth)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_BORDER_WIDTH, 
-        JSON.stringify(borderWidth))
-    }, [borderWidth]);
-
-    useEffect(() => {
-        const storedBorderColor = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_BORDER_COLOR))
-        if (storedBorderColor) setBorderColor(storedBorderColor)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_BORDER_COLOR, 
-        JSON.stringify(borderColor))
-    }, [borderColor]);
-
-    useEffect(() => {
-        const storedColor = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_COLOR))
-        if (storedColor) setColor(storedColor)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_COLOR, 
-        JSON.stringify(color))
-    }, [color]);
-
-    useEffect(() => {
-        const storedBackgroundColor = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_BACKGROUND))
-        if (storedBackgroundColor) setBackgroundColor(storedBackgroundColor)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_BACKGROUND, 
-        JSON.stringify(backgroundColor))
-    }, [backgroundColor]);
-
-    useEffect(() => {
-        const storedImage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_CUSTOM_IMAGE))
-        if (storedImage) setMediaPreview(storedImage)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_CUSTOM_IMAGE, 
-        JSON.stringify(mediaPreview))
-    }, [mediaPreview]);
-
-    useEffect(() => {
-        const storedImageWidth = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IMAGE_WIDTH))
-        if (storedImageWidth) setImageWidth(storedImageWidth)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_IMAGE_WIDTH, 
-        JSON.stringify(imageWidth))
-    }, [imageWidth]);
-
-    useEffect(() => {
-        const storedImageHeight = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IMAGE_HEIGHT))
-        if (storedImageHeight) setImageHeight(storedImageHeight)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_IMAGE_HEIGHT, 
-        JSON.stringify(imageHeight))
-    }, [imageHeight]);
-
-    useEffect(() => {
-        const storedImageRotation = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IMAGE_ROTATION))
-        if (storedImageRotation) setImageRotation(storedImageRotation)
-      }, [])
-    
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY_IMAGE_ROTATION, 
-        JSON.stringify(imageRotation))
-    }, [imageRotation]);
-
     return (
         <>
             <Head>
                 <title>Custom Advertisement Generator</title>
                 <meta name="description" content="earnandtrade, advertisement" />
             </Head>
+            <LocalCustom company={company} setCompany={setCompany} companyFontSize={companyFontSize} setCompanyFontSize={setCompanyFontSize} companyFontWeight={companyFontWeight} setCompanyFontWeight={setCompanyFontWeight} description={description} setDescription={setDescription} descriptionFontSize={descriptionFontSize} setDescriptionFontSize={setDescriptionFontSize} descriptionFontWeight={descriptionFontWeight} setDescriptionFontWeight={setDescriptionFontWeight} borderWidth={borderWidth} setBorderWidth={setBorderWidth} borderColor={borderColor} setBorderColor={setBorderColor} color={color} setColor={setColor} backgroundColor={backgroundColor} setBackgroundColor={setBackgroundColor} mediaPreview={mediaPreview} setMediaPreview={setMediaPreview} image={image} setImage={setImage} imageWidth={imageWidth} setImageWidth={setImageWidth} imageHeight={imageHeight} setImageHeight={setImageHeight} imageRotation={imageRotation} setImageRotation={setImageRotation} />
             <Container 
                 size="massive" 
                 style={{ 
