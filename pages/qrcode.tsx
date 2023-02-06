d3ac02a43d14ae7e1875127aff3a5ac53ab129ad
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2023-02-05 23:50:55 -0800

    qr code edits

--- pages/qrcode.tsx
+++ pages/qrcode.tsx
@@ -1,18 +1,144 @@
 import React from 'react';
 import { QRCode } from 'react-qrcode-logo';
 
-
 export default function qrCode() {
     return (
         <>
+            <div
+                style={{
+                    color: '#125CA1',
+                    cursor: 'pointer',
+                    fontSize: '30px',
+                    margin: '20px 0px 10px 0px'
+                }}
+            >
+                <a href='https://www.testandgrade.com'>
+                    TestAndGrade.com
+                </a>
+            </div>
             <div>
                 <QRCode 
-                    value="https://www.testandgrade.com" 
+                    value='https://www.testandgrade.com'
                     logoImage='/images/test1.png'
-                    size={290}
-                    logoWidth={80}
+                    // size={290}
+                    // logoWidth={60}
                     eyeColor='#125CA1'
                     fgColor='#125CA1'
+                    removeQrCodeBehindLogo={true}
+                />
+            </div>
+            <div
+                style={{
+                    color: '#125CA1',
+                    cursor: 'pointer',
+                    fontSize: '30px',
+                    margin: '20px 0px 10px 0px'
+                }}
+            >
+                <a href='https://www.antinodelocation.com'>
+                    AntinodeLocation.com
+                </a>
+            </div>
+            <div>
+                <QRCode
+                    value='https://www.antinodelocation.com'
+                    logoImage='/images/antinode_logo.png'
+                    // size={290}
+                    // logoWidth={60}
+                    eyeColor='#125CA1'
+                    fgColor='#125CA1'
+                    removeQrCodeBehindLogo={true}
+                />
+            </div>
+            <div
+                style={{
+                    color: '#999999',
+                    cursor: 'pointer',
+                    fontSize: '30px',
+                    margin: '20px 0px 10px 0px'
+                }}
+            >
+                <a href='https://cssclone.com'>
+                    cssClone.com
+                </a>
+            </div>
+            <div>
+                <QRCode
+                    value='https://cssclone.com'
+                    logoImage='/images/apple1.png'
+                    // size={290}
+                    // logoWidth={60}
+                    eyeColor='#999999'
+                    fgColor='#999999'
+                    removeQrCodeBehindLogo={true}
+                />
+            </div>
+            <div
+                style={{
+                    color: '#13a557',
+                    cursor: 'pointer',
+                    fontSize: '30px',
+                    margin: '20px 0px 10px 0px'
+                }}
+            >
+                <a href='https://www.advertisementgenerator.com'>
+                    AdvertisementGenerator.com
+                </a>
+            </div>
+            <div>
+                <QRCode
+                    value='https://www.advertisementgenerator.com'
+                    logoImage='/images/E&T_logo.png'
+                    // size={290}
+                    // logoWidth={60}
+                    eyeColor='#13a557'
+                    fgColor='#13a557'
+                    removeQrCodeBehindLogo={true}
+                />
+            </div>
+            <div
+                style={{
+                    color: 'black',
+                    cursor: 'pointer',
+                    fontSize: '30px',
+                    margin: '20px 0px 10px 0px'
+                }}
+            >
+                <a href='https://www.'>
+                    HealthStat.com
+                </a>
+            </div>
+            <div>
+                <QRCode
+                    // value='https://'
+                    logoImage='/images/healthstat.png'
+                    // size={290}
+                    // logoWidth={60}
+                    eyeColor='black'
+                    fgColor='black'
+                    removeQrCodeBehindLogo={true}
+                />
+            </div>
+            <div
+                style={{
+                    color: 'black',
+                    cursor: 'pointer',
+                    fontSize: '30px',
+                    margin: '20px 0px 10px 0px'
+                }}
+            >
+                <a href='https://github.com/Jbertalli'>
+                    Jbertalli GitHub
+                </a>
+            </div>
+            <div>
+                <QRCode
+                    // value='https://'
+                    logoImage='/images/GitHub_logo.png'
+                    // size={290}
+                    // logoWidth={60}
+                    eyeColor='black'
+                    fgColor='black'
                     removeQrCodeBehindLogo={true}
                 />
             </div>
