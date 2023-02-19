95b320cd932808e5fa3d9eead567249bd14a180c
Julian Bertalli <87340383+Jbertalli@users.noreply.github.com>
2023-02-19 15:55:30 -0800

    protected route!!!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,34 @@ import Head from 'next/head';
 import Layout from '../components/Layout';
 import { store } from '../store';
 import { Provider } from 'react-redux';
+import { useRouter } from 'next/router';
+import { auth } from '../firebase/clientApp';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import React, { useEffect } from 'react';
+
+auth;
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  console.log(user);
+
+  useEffect(() => {
+    console.log(window.document.cookie.length);
+    if (user || window.document.cookie.length > 17) {
+      console.log('Signed In');
+    } else if (user === null && window.document.cookie.length === 17) {
+      router.push('/');
+      console.log('Cannot access this page without logging in')
+    } else {
+      console.log('done');
+    }
+  }, [user]);
+
+  if (user === null && router.pathname !== '/') {
+    return null;
+  }
+
   return (
     <>
       <Head>
